5046a0245317fcf0990a0d58c9f64d1884d44c45
shenry45 <shawn45henry@gmail.com>
2019-08-29 22:53:57 -0700

    add tests to GET and DELETE, note... Jest does not accept URL params in method

--- __tests__/gamesTest.js
+++ __tests__/gamesTest.js
@@ -9,11 +9,12 @@ describe('games', () => {
   // clear db before tests
   beforeEach(async () => {
     await db('games').truncate();
-  })
+  });
 
+  // check db test env
   it('server in testing environment', () => {
     expect(process.env.DB_ENV).toBe('testing');
-  })
+  });
 
   // get games from db
   it('should return JSON games list', async () => {
@@ -24,21 +25,48 @@ describe('games', () => {
         expect(res.status).toBe(200);
       })
   });
+  //
 
   // add game to db for delete test
   it('should add game', async () => {
-      await db('games').insert({name: 'MW2'});
+      await request(server)
+        .post('/add')
+        .send({name: 'MW2'})
+        .then(async (res) => {
+          const testGame = await db('games').where('name', 'MW2');
+          expect(testGame).toHaveLength(1);
+        })
+      
+      // db('games').insert({name: 'MW2'});
+  });
+  // make sure if no req key "name" will fail
+  it('fails to add game', async () => {
+    await request(server)
+      .post('/add')
+      .send()
+      .then(res => {
+        expect(res.status).toBe(500);
+      })
+  });
 
-      const testGame = await db('games').where('name', 'MW2');
-      expect(testGame).toHaveLength(1);
-  })
-
-  // delete specific game by ID from db
+  // make sure if no req key "id" will fail
   it('should delete specific game', async () => {
-    const id = await db.select('id').from('games').where('name', 'MW2');
-    
-    await db('games').where('id', id).delete()
-    
+    // req param placeholder not accepted by Jest
+    await request(server)
+      .delete('/')
+      .send()
+      .then(res => {
+        expect(res.status).toBe(404);
+      })
+  });
+  // make sure no body is returned
+  it('should delete and return no body', async () => {
+    await request(server)
+      .delete('/')
+      .send({id: '1'})
+      .then(res => {
+        expect(res.body).toStrictEqual({});
+      })
   })
 });
 
